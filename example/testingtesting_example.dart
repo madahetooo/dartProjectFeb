e4295d3a8fdb64f05b56db5f50252867c2cc0698
madahetooo <ieslammedhat@gmail.com>
2023-02-01 22:57:23 +0200

    Initial commit

---
+++ example/testingtesting_example.dart
@@ -0,0 +1,6 @@
+import 'package:testingtesting/testingtesting.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/src/main.dart
@@ -0,0 +1,79 @@
+void main() {
+  // // //Declare name variable
+  // var name = "Eslam Medhat";
+  // var age = 27;
+  // print("Your name is $name and your age is $age");
+
+  // var x = 5;
+  // var y = 10;
+  // print(x+y);
+  // print(x-y);
+  // print(x/y);
+  // print(x*y);
+
+  // var x = 17;
+  // var y = 15;
+  // var z = 20;
+  //
+  // if (x == 17) {
+  //   print("X is  equal to 17");
+  // }
+
+  // var x = 5;
+  // // print(x++); //5
+  // // print(x); //6
+  // print(--x); //4
+
+  // var x = 5;
+  // // x += 1 ;   // x = x + 1
+  // x -=1 ;
+  // print(x);
+
+  // var x = 2;
+  // var result = (x > 5 ) ? "X is above 5"  : "X is below 5" ;
+  // print(result);
+
+  // var x = 6;
+  // if (x < 1) {
+  //   print("X is lower than 1");
+  // }
+  // else if (x == 5) {
+  //   print("X is equal to 5");
+  // }
+  // else{
+  //   print("X is not equal to 5");
+  // }
+
+  // var x = 5;
+  // switch (x) {
+  //   case 1:
+  //     print("X is equal to 1");
+  //     break;
+  //   case 2:
+  //     print("X is equal to 2");
+  //     break;
+  //   case 3:
+  //     print("X is equal to 3");
+  //     break;
+  //   case 4:
+  //     print("X is equal to 4");
+  //     break;
+  //   default:
+  //     {
+  //       print("X is not vaild number");
+  //     }
+  // }
+
+  // var x = 5;
+  // if (x is int) {
+  //   print("x is int");
+  // }
+
+  var x = 7;
+  var y = 6;
+  var z = 8;
+
+  if (x != z) {
+    print("X is the not equal to z number");
+  }
+}
